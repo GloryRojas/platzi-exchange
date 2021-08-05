3ff30673b3b88cd934f037845cb2b699b3ad187a
glory.rojas <grojasm94@gmail.com>
2021-08-04 19:01:37 -0500

    feat: add filters to normalize numbers

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -21,7 +21,7 @@
       >
         <td>
           <img
-              class="w-6 h-6"
+            class="w-6 h-6"
             :src="`https://static.coincap.io/assets/icons/${a.symbol.toLowerCase()}@2x.png`"
             :alt="a.name"
           />
@@ -33,13 +33,13 @@
           {{ a.name }}
         </td>
         <td>
-          {{ a.priceUsd }}
+          {{ a.priceUsd | dollar }}
         </td>
         <td>
-          {{ a.marketCapUsd }}
+          {{ a.marketCapUsd | dollar }}
         </td>
-        <td>
-          {{ a.changePercent24Hr }}
+        <td :class="a.changePercent24Hr.includes('-') ? 'text-red-600' : 'text-green-600'">
+          {{ a.changePercent24Hr | percent }}
         </td>
         <td class="hidden sm:block"></td>
       </tr>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
   </div>
 </template>
 <script>
-import api from "../api";
+import api from '../api'
 
 import PxAssetsTable from '../components/PxAssetsTable'
 
